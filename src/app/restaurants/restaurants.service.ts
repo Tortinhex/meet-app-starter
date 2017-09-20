3a6a36d448ac79577bf1a333b33e2ae33fe5f4c8
Danilo Dorotheu <d.oliveira.dorotheu@avanade.com>
2017-09-20 19:53:32 -0300

    - Adicionando o servico de carrinho; - Adicionando os eventos de adicao e remocao de itens no carrinho; - Adicao do exemplo de compartilhamento de funcoes entre componentes irmaos; - Corrigindo o rating (O range do rating estava errado)

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import { Restaurant } from './restaurant/restaurant.model';
+import { MenuItem } from './../restaurant-detail/menu-item/menu-item.model';
 import { MEAT_API } from './../app.api';
 import { ErrorHandler } from '../app.error-handler';
 
@@ -43,6 +44,13 @@ export class RestaurantsService {
 	reviewsOfRestaurant(id: string): Observable<any> {
 		return this.http
 			.get(`${MEAT_API}/restaurants/${id}/reviews`)
+			.map(response => response.json())
+			.catch(ErrorHandler.handleError);
+	}
+
+	menuOfRestaurant(id: string): Observable<MenuItem[]> {
+		return this.http
+			.get(`${MEAT_API}/restaurants/${id}/menu`)
 			.map(response => response.json())
 			.catch(ErrorHandler.handleError);
 	}
