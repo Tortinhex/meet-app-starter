aab8da99aa0c2244c5b2462ef9fa6c5ae342b2fe
Danilo Dorotheu <d.oliveira.dorotheu@avanade.com>
2017-09-20 17:22:32 -0300

    - Criando o errorHandler para requisicoes e incluindo no restaurants.service

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import { Restaurant } from './restaurant/restaurant.model';
-import 'rxjs/add/operator/map';
 import { MEAT_API } from './../app.api';
+import { ErrorHandler } from '../app.error-handler';
+
 import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 
 @Injectable()
 export class RestaurantsService {
@@ -18,7 +21,8 @@ export class RestaurantsService {
 	restaurants(): Observable<Restaurant[]> {
 		return this.http
 			.get(`${MEAT_API}/restaurants`)
-			.map(response => response.json());
+			.map(response => response.json())
+			.catch(ErrorHandler.handleError);
 	}
 
 }
