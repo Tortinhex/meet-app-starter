4e09625b0fe08d6d07a8d78d7dc58acc1ec4eaae
Danilo Dorotheu <d.oliveira.dorotheu@avanade.com>
2017-09-20 21:18:10 -0300

    - Incluindo o componente de pedido;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,7 @@ import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component'
 
 import { RestaurantsService } from './restaurants/restaurants.service';
 import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-cart.service';
+import { OrderComponent } from './order/order.component';
 
 @NgModule({
     declarations: [
@@ -32,7 +33,8 @@ import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-
         MenuComponent,
         ShoppingCartComponent,
         MenuItemComponent,
-        ReviewsComponent
+        ReviewsComponent,
+        OrderComponent
     ],
     imports: [
         BrowserModule,
