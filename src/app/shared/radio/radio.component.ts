c0abb8d6de71674f9f6cdf9069b322dd213a35da
Danilo Dorotheu <d.oliveira.dorotheu@avanade.com>
2017-09-21 12:36:51 -0300

    - Implementando o accessor do componente radio para permitir a validacao dos valores do lado de fora do componente

--- src/app/shared/radio/radio.component.ts
+++ src/app/shared/radio/radio.component.ts
@@ -1,15 +1,25 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, forwardRef } from '@angular/core';
+import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
 import { RadioOption } from './radio-option.model';
 
 @Component({
     selector: 'mt-radio',
-    templateUrl: './radio.component.html'
+    templateUrl: './radio.component.html',
+    providers: [
+        {
+            provide: NG_VALUE_ACCESSOR,
+            useExisting: forwardRef(() => RadioComponent),
+            multi: true
+        }
+    ]
 })
-export class RadioComponent implements OnInit {
+export class RadioComponent implements OnInit, ControlValueAccessor {
 
     @Input() options: RadioOption[];
 
     value: any;
+
+    onChange: any;
 
     constructor() { }
 
@@ -18,6 +28,29 @@ export class RadioComponent implements OnInit {
 
     setValue(value: any): void {
         this.value = value;
+        this.onChange(this.value);
     }
+
+    /**
+     * Write a new value to the element
+     * Chamado pelas diretivas quando ele quer passar um valor ao componente
+     * @param obj 
+     */
+    writeValue(obj: any): void {
+        this.value = obj;
+    }
+
+    /**
+     * Set the function to be called when the control receives a change event
+     * Passa uma funcao que tem q ser chamada sempre que o valor do componente mudar
+     * @param fn 
+     */
+    registerOnChange(fn: any): void {
+        this.onChange = fn;
+    }
+
+    registerOnTouched(fn: any): void {}
+
+    setDisabledState?(isDisabled: boolean): void {}
 
 }
